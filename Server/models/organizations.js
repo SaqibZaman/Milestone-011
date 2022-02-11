3c1e2de7c1948357ca34a2b093b46f6769b8935b
Saqib Zaman <saqibzaman619@gmail.com>
2022-02-11 20:01:55 +0500

    moved services to another directory

--- Server/models/organizations.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const Organization = mongoose.Schema({
-    brand_uuid:[],
-    org_uuid:{type: String},
-    outlet_uuid:[],
-})
-
-module.exports = mongoose.model("organization", Organization);
--- Server/server.js
+++
@@ -1,6 +0,0 @@
-const http = require("http");
-const app = require("./app");
-
-const server = http.createServer(app)
-
-app.listen(8000, () => console.log("Server running on port 8000"));
